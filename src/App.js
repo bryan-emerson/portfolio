d1e02e8833e9304a0051ad10d5aa8ac88d3d8132
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-05-25 14:44:50 -0400

    nav bar layout as desired

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Nav from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Nav';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Sup tho</h1>
-      <Nav />
+      <Navigation />
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './style.css'
 
 
-export default class Nav extends Component {
+export default class Navigation extends Component {
   render() {
     return (
-      <Navbar className="nav">
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home" className="name">Bryan Emerson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Container className="links-container">
+          <Nav className="links">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link1">Link</Nav.Link>
+            <Nav.Link href="#link2">Link</Nav.Link>
+          </Nav>
+          </Container>
         </Navbar.Collapse>
       </Navbar>
     )
