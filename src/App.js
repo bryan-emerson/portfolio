38bd41919c2bd5cbe9c6cce3e356e066ca45c38f
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-05-28 12:27:22 -0400

    react router instantiated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Navigation from './components/Nav';
 // import HomeImage from './components/HomeImage';
 import HomePage from './pages/HomePage'
+import About from './pages/About'
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
