69dbab968201e00202f864f7b49b0e656c1925a1
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-06-01 13:03:14 -0400

    props passed from projects page to projcard component

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,19 +3,20 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 export default class ProjectCard extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+
+  // }
 
   render() {
+    console.log(this.props.projObj)
     return (
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{this.props.projObj.name}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+          {this.props.projObj.info}
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import ProjectCard from '../../components/ProjectCard/index.js'
 
 export default class Projects extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
+
 
 
   render() {
+
+    let proj = {
+      name: 'project 1',
+      info: 'bunch of text about proj',
+      url: 'www.google.com'
+    }
+
     return (
       <div>
         <h1>what's good</h1>
-        <ProjectCard />
+        <ProjectCard projObj={proj}/>
       </div>
     )
   }
