de4c9abaa1c19385d17136c0ccab2eae1c7207b1
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-06-15 17:45:35 -0400

    implemented component loop and is rendering properly

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,20 +3,28 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 export default class ProjectCard extends Component {
-  // constructor(props) {
-  //   super(props);
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
 
-  // }
+  }
+
+  getAllProj = (array) => {
+    for (let i = 0; i < array; i++) {
+      console.log(i)
+    }
+  }
 
   render() {
-    console.log(this.props.projObj)
+    // this.getAllProj(this.props.projObj)
+    // console.log(this.props.projObj)
     return (
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
           <Card.Title>{this.props.projObj.name}</Card.Title>
           <Card.Text>
-          {this.props.projObj.info}
+            {this.props.projObj.info}
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -2,24 +2,67 @@ import React, { Component } from 'react';
 import ProjectCard from '../../components/ProjectCard/index.js'
 
 export default class Projects extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        {
+          name: 'project 1',
+          info: 'bunch of text about proj',
+          url: 'www.google.com'
+        },
+        {
+          name: 'project 2',
+          info: 'bunch of text about proj 2',
+          url: 'www.google.com'
+        },
+        {
+          name: 'project 3',
+          info: 'bunch of text about proj',
+          url: 'www.google.com'
+        },
+        {
+          name: 'project 4',
+          info: 'bunch of text about proj 2',
+          url: 'www.google.com'
+        },
+        {
+          name: 'project 5',
+          info: 'bunch of text about proj',
+          url: 'www.google.com'
+        },
+        {
+          name: 'project 6',
+          info: 'bunch of text about proj 2',
+          url: 'www.google.com'
+        },
+      ]
+    };
+  }
 
+  getAllProj = (array, index) => {
+    let tempArr = [];
+    for (let i = 0; i < array.length; i++) {
+      tempArr.push(<ProjectCard projObj={array[i]} key={i}/>)
+    }
+    return tempArr;
+  }
 
 
   render() {
+    console.log(this.state.projects)
 
-    let proj = {
-      name: 'project 1',
-      info: 'bunch of text about proj',
-      url: 'www.google.com'
+    let allProjects = "";
+
+    if (this.state.projects.length) {
+      allProjects = this.getAllProj(this.state.projects)
     }
-
     return (
       <div>
         <h1>what's good</h1>
-        <ProjectCard projObj={proj}/>
+        {/* <ProjectCard projObj={this.state.projects}/> */}
+        {allProjects}
       </div>
     )
   }
