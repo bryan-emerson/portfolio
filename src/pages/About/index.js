f293077bb8ce1695f7958b8e25c2ab6c1c2ea42a
Bryan Emerson <bryan.l.emerson@gmail.com>
2022-10-31 12:29:34 -0700

    title change

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -14,7 +14,7 @@ export default class About extends Component {
         <Image src={hike} alt="portrait of Bryan" className="img-fluid" align='right'/>
         </Container>
         <div className='words'>
-        <p>My Name Is Bryan Emerson! I'm a Fullstack Software Engineer with a penchant for clean code and user friendly solutions. I strive to create websites that meet client needs and expectations.
+        <p>My Name Is Bryan Emerson! I'm a Frontend Software Engineer with a penchant for clean code and user friendly solutions. I strive to create websites that meet client needs and expectations.
         </p>
         <p>My technical foundation was built on eleven years of aircraft maintenance while working as a Crew Chief in the United States Air Force. My ability to effectively  communicate comes from extensive experience in the hospitality sector, working positions ranging from bartender, to consultant, to owner/operator. These roles fortified my strong communication skills, enthusiasm for both customer and team relations, and attention to detail. These combined experiences in technical maintenance and customer relations forged an exemplary foundation for being an effective software engineer.
         </p>
